4b1ddbb40cda8ad8995803bd982583cade2c35b6
Jamie Pate <jamie.pate@gmail.com>
2013-09-16 01:49:59 -0700

    Alter number validators to parse the attribute value every time so that expressions are able to be used.

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -541,8 +541,8 @@ function numberInputType(scope, element, attr, ctrl, $sniffer, $browser) {
   });
 
   if (attr.min) {
-    var min = parseFloat(attr.min);
     var minValidator = function(value) {
+      var min = parseFloat(attr.min);
       if (!isEmpty(value) && value < min) {
         ctrl.$setValidity('min', false);
         return undefined;
@@ -557,8 +557,8 @@ function numberInputType(scope, element, attr, ctrl, $sniffer, $browser) {
   }
 
   if (attr.max) {
-    var max = parseFloat(attr.max);
     var maxValidator = function(value) {
+      var max = parseFloat(attr.max);
       if (!isEmpty(value) && value > max) {
         ctrl.$setValidity('max', false);
         return undefined;
